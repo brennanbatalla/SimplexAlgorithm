c010b30698d02da158f8103d25f9ea18c0c08966
xsorifc28 <samed@xsorcreations.com>
2014-12-20 19:07:39 -0500

    Syling edits

--- app/src/main/java/bgb/com/simplexalgorithm/InputActivity.java
+++ app/src/main/java/bgb/com/simplexalgorithm/InputActivity.java
@@ -1,5 +1,6 @@
 package bgb.com.simplexalgorithm;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,7 +10,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -24,28 +24,29 @@ public class InputActivity extends ActionBarActivity {
     MaterialEditText numCon;
     LinearLayout radioButton;
     Button inputButton;
-    RadioGroup matoreq;
-    RadioGroup minormaxG;
+    RadioGroup rg_inputMode;
+    RadioGroup rg_minMax;
+
+	final String TAG = "InputActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        initialize();
+        Initialize();
 
         eqButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("InputButtonlistener", "HERE");
+
+                Log.i(TAG, "OnClickListener/eqButton");
 
                 if(numVar.getText().toString().matches("")) {
 	                numVar.setError("Missing number of variables!");
 	            } else if(numCon.getText().toString().matches("")) {
 	                numCon.setError("Missing number of constraints!");
                 } else {
-	                int numCol = Integer.parseInt(numVar.getText().toString());
-	                int numRow = Integer.parseInt(numCon.getText().toString());
 	                radioButton.setVisibility(View.VISIBLE);
                 }
             }
@@ -55,61 +56,42 @@ public class InputActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
 
-                boolean matoreqq = MATorEQ();
-                boolean minormax = minORmax();
+	            Intent i = new Intent();
 
-//                if(matoreqq)
-//                Log.e("InputActivityPlusButton", "True");
-//                else
-//                    Log.e("InputActivityPlusButton", "False");
-//
-//                if(minormax)
-//                    Log.e("InputActivityPlusButton", "min");
-//                else
-//                    Log.e("InputActivityPlusButton", "max");
+	            // Get input mode radio selection
+	            int inputMode = rg_inputMode.getCheckedRadioButtonId();
 
-
-                    // Matrix Input and Min
-                if(matoreqq == true && minormax == true){
-
-                    Intent myIntent = new Intent(getApplicationContext(), MatrixInput.class);
-                    myIntent.putExtra("numVar", numVar.getText().toString());
-                    myIntent.putExtra("numCon", numCon.getText().toString());
-                    myIntent.putExtra("minOrmax", minormax);
+	            if(inputMode == -1) {
+	                Log.e(TAG,"inputMode/No selection");
+		            Cheers("Please choose an input mode.");
+	            } else if (inputMode == R.id.rb_tableauMatrix) {
+		            Log.i(TAG,"inputMode/Maximum");
+		            i = new Intent(getApplicationContext(), MatrixInput.class);
+	            } else if (inputMode == R.id.rb_equationInput) {
+		            Log.i(TAG,"inputMode/Minimum");
+		            i = new Intent(getApplicationContext(), MatrixInput.class);
+	            } else {
+					Cheers("An unknown error has occurred");
+	            }
 
 
-                    startActivity(myIntent);
-                    Log.e("InputActivityPlusButton", "Started New Activity?");
-                }
-                // Matrix Input and Max
-                else if(matoreqq == true && minormax == false){
+	            // Get solve mode radio selection
+				int solveMode = rg_minMax.getCheckedRadioButtonId();
 
-                    Intent myIntent = new Intent(getApplicationContext(), MatrixInput.class);
-                    myIntent.putExtra("numVar", numVar.getText().toString());
-                    myIntent.putExtra("numCon", numCon.getText().toString());
-                    myIntent.putExtra("minOrmax", minormax);
-                    startActivity(myIntent);
-                }
+	            if(solveMode == -1)  {
+		            Log.e(TAG,"solveMode/No selection");
+		            Cheers("Please choose a solve mode.");
+	            } else if (solveMode == R.id.rb_max) {
+		            Log.i(TAG,"inputMode/Maximum");
+		            i.putExtra("solveMode","max");
+	            } else if (solveMode == R.id.rb_min) {
+		            Log.i(TAG,"inputMode/Minimum");
+		            i.putExtra("solveMode","min");
+	            } else {
+		            Cheers("An unknown error has occurred");
+	            }
 
-                // Equation Input and Min
-                else if(matoreqq == false && minormax == true){
-
-                    Intent myIntent = new Intent(getApplicationContext(), EquationInput.class);
-                    myIntent.putExtra("numVar", numVar.getText().toString());
-                    myIntent.putExtra("numCon", numCon.getText().toString());
-                    myIntent.putExtra("minOrmax", minormax);
-                    startActivity(myIntent);
-
-                }
-                // Equation Input and Max
-                else{
-                    Intent myIntent = new Intent(getApplicationContext(), EquationInput.class);
-                    myIntent.putExtra("numVar", numVar.getText().toString());
-                    myIntent.putExtra("numCon", numCon.getText().toString());
-                    myIntent.putExtra("minOrmax", minormax);
-                    startActivity(myIntent);
-
-                }
+	            startActivity(i);
 
             }
         });
@@ -139,52 +121,19 @@ public class InputActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void initialize(){
+    public void Initialize(){
         eqButton = (FloatingActionButton) findViewById(R.id.fab);
         numVar = (MaterialEditText) findViewById(R.id.et_num_variables);
         numCon = (MaterialEditText) findViewById(R.id.et_num_constraints);
         radioButton = (LinearLayout) findViewById(R.id.modeSelection);
         inputButton = (Button) findViewById(R.id.goToInputsButton);
-        matoreq = (RadioGroup) findViewById(R.id.matoreqGroup);
-        minormaxG = (RadioGroup) findViewById(R.id.minmaxGroup);
+        rg_inputMode = (RadioGroup) findViewById(R.id.matoreqGroup);
+        rg_minMax = (RadioGroup) findViewById(R.id.minmaxGroup);
 
     }
 
-    public boolean MATorEQ(){
-        boolean tf = false;
-        int id = matoreq.getCheckedRadioButtonId();
-        RadioButton RB = (RadioButton)findViewById(id);
 
-            Log.e("MatEqRG", RB.getText().toString());
-
-
-        if(RB.getText().toString().matches("Matrix Input"))
-            tf = true;
-        else
-        tf = false;
-
-    return tf;
-    }
-
-
-    public boolean minORmax() {
-        boolean tf = false;
-        int id = minormaxG.getCheckedRadioButtonId();
-        RadioButton RB = (RadioButton)findViewById(id);
-
-        Log.e("minORmax", RB.getText().toString());
-
-
-        if(RB.getText().toString().matches("Minimum"))
-            tf = true;
-        else
-            tf = false;
-
-        return tf;
-
-    }
-
-    public void cheers(String message){
+    public void Cheers(String message){
 
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_LONG).show();
