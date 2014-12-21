d7be5a61e0e1e28a03a75ac1d24eefd396ff5700
xsorifc28 <samed@xsorcreations.com>
2014-12-20 20:41:25 -0500

    Syling edits

--- app/src/main/java/bgb/com/simplexalgorithm/EquationInput.java
+++ app/src/main/java/bgb/com/simplexalgorithm/EquationInput.java
@@ -7,8 +7,6 @@ import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.ContextThemeWrapper;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
@@ -205,23 +203,6 @@ public class EquationInput extends ActionBarActivity {
 		Cheers(solution);
 		Cheers("Optimal: " + optimalValue);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_simplexmain, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-
-		return super.onOptionsItemSelected(item);
-    }
-
 
 	public void displayFunctionInputs(int c){
 
